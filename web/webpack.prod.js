7ef7c6f8850a0854d9dec04b4cbff15d1ccc4fd6
Julien Brochet <julien@aerialls.io>
2019-04-17 14:10:10 +0200

    Use system var for production deployment

--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -1,10 +1,15 @@
-const SriPlugin = require('webpack-subresource-integrity')
+const SriPlugin = require('webpack-subresource-integrity');
 const merge = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
+    new Dotenv({
+      systemvars: true,
+      silent: true,
+    }),
     new SriPlugin({
       hashFuncNames: ['sha256', 'sha384'],
     }),
