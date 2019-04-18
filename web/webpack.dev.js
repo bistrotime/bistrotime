9141f683be8545a132a5590421eeec9901fcfe92
Julien Brochet <julien@aerialls.io>
2019-04-18 20:20:36 +0200

    Use port 9000 for the web app in dev mode

--- web/webpack.dev.js
+++ web/webpack.dev.js
@@ -1,12 +1,15 @@
 const merge = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
   },
   plugins: [
     new Dotenv(),
