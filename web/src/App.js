dfbc5e1daaf240b40c47f6dc593536fc28a9556b
Julien Brochet <julien@aerialls.io>
2019-05-28 19:56:32 +0200

    [web] Migrate material-ui to v4

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import JssProvider from 'react-jss/lib/JssProvider';
-import { create } from 'jss';
 import { SnackbarProvider } from 'notistack';
 
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import { StylesProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Bistrotime from './bistrotime';
 
-const generateClassName = createGenerateClassName();
-const jss = create({
-  ...jssPreset(),
-  insertionPoint: 'jss-insertion-point',
-});
-
 const App = () => ((
-  <JssProvider jss={jss} generateClassName={generateClassName}>
+  <StylesProvider injectFirst>
     <SnackbarProvider maxSnack={3}>
       <CssBaseline />
       <Bistrotime />
     </SnackbarProvider>
-  </JssProvider>
+  </StylesProvider>
 ));
 
 export default App;
